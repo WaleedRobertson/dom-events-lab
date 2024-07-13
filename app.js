8bdced91f8afc6b89849bfb58b1841bf8a498106
Waleed Robertson <waleedrbrtsn1@email.com>
2024-07-13 19:49:48 -0400

    Restarting the dom-events-lab

---
+++ app.js
@@ -0,0 +1,36 @@
+
+// // As a user, I want to be able to select numbers so that I can perform operations with them.
+// // As a user, I want to be able to add two numbers together.
+// additionResult = firstNumberInput + secondNumberInput
+// // As a user, I want to be able to subtract one number from another.
+// subtractionResult = firstNumberInput - secondNumberInput
+// // As a user, I want to be able to multiply two numbers together.
+// multiplicationResult = firstNumberInput * secondNumberInput
+// // As a user, I want to be able to divide one number by another.
+// divisionResult = firstNumberInput / secondNumberInput
+// // As a user, I want to be able to see the output of the mathematical operation.
+// // As a user, I want to be able to clear all operations and start from 0.
+// /*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+let calculatorResultDisplay = document.querySelector('.display');
+
+/*-------------------------------- Variables --------------------------------*/
+let firstNumberInput;
+let secondNumberInput;
+let additionResult;
+let subtractionResult;
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+    
+     });
+  });
+/*-------------------------------- Functions --------------------------------*/
+divisionResult = firstNumberInput / secondNumberInput
+multiplicationResult = firstNumberInput * secondNumberInput
+subtractionResult = firstNumberInput - secondNumberInput
+additionResult = firstNumberInput + secondNumberInput
